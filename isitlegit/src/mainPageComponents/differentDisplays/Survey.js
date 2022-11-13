fa7a95e5339b33d2ec1f2e12311e2243eefd6a5e
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-11-12 22:46:13 -0600

    add css to scroll

--- isitlegit/src/mainPageComponents/differentDisplays/Survey.js
+++ isitlegit/src/mainPageComponents/differentDisplays/Survey.js
@@ -1,25 +1,33 @@
 import React from "react";
+
 export default function Home(){
-    let [ratingOne, setRatingOne] = React.useState(true)
-    let [ratingTwo, setRatingTwo] = React.useState(false)
-    let [ratingThree, setRatingThree] = React.useState(false)
-    let [ratingFour, setRatingFour] = React.useState(false)
-    let [ratingFive, setRatingFive] = React.useState(false)
+    let [rating, setRatingOne] = React.useState(0)
+
     return(
         <div className="surveyInput">
             <div>What is your Name: </div>
             <input type="text" id="name" className="nameInput" />
             <div>Leave a Rating:</div>
+            <br />
 
-            <span class="checked">✭</span>
-            <span onClick={() => (setRatingTwo(true), setRatingThree(false), setRatingFour(false), setRatingFive(false))}>✭</span>
+            {/*}<span class="checked">✭</span>
+            <span onClick={() => (setRatingTwo(true), setRatingThree(false), setRatingFour(false), setRatingFive(false))}>✭</span> */}
+            <div>
+            <span class="surveyInputValue">0</span>
+            <span class="surveyInputValue">1</span>
+            <span class="surveyInputValue">2</span>
+            <span class="surveyInputValue">3</span>
+            <span class="surveyInputValue">4</span>
+            <span class="">5</span>
+            </div>
+            <input className = "isInput" type="range" min="0" max="5" />
             <div>Any Additional Comments: </div>
             <textarea id="comments" className="textarea"/>
             <br />
-            <button class="btn btn-primary postButton" > Submit </button>
+            <button className="btn btn-success postButton" > Submit </button>
             {/* onClick = {() => goToStart()} */}
             <br />
             {/* <button onClick={() => validateSignIn()}>Click to sign in</button> */}
-        </div>
+      </div>
     )
 }

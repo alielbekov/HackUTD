98dd68e82b2d0c6e3edcfafbfe44c6bf47cfff61
rahiblaghari <rlaghari@gmail.com>
2022-11-13 08:36:50 -0600

    more functionality

--- isitlegit/src/mainPageComponents/differentDisplays/TrackSentiment.js
+++ isitlegit/src/mainPageComponents/differentDisplays/TrackSentiment.js
@@ -2,35 +2,54 @@ import React from "react";
 import Chart from 'chart.js/auto';
 import {Line} from 'react-chartjs-2'
 
+// let holder = "abc";
+
 export default function TrackSentiment(){
 
+    let [holder, setHolder] = React.useState("abc");
+
+    function getTheChart(input){
+        switch(input){
+            case('CompanyA'):
+                return [88,80,96,83,68,98]
+            case('CBRE'):
+                return [88,95,96,98,99,100]
+            case('@brittneySpears'):
+                return [85,82,80,83,83,85]
+            case('@steven'):
+                return [90,20,10,90,20,10]
+            case('Apple'):
+                return [90,80,70,60,70,98]
+            default:
+                return [88,95,96,83,68,98]
+        }
+    }
+
     return(
-      <div className="chart">
-              <Line width={50} height = {20}
-              data= {{
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [3,10,20,11,13,17],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }}/>
-  </div>
+      <div>
+          <div className="titleText">
+                 Find a user or company to search for:
+                 <div>
+                     <input type="text" name="search" id="search" className="search" placeholder="Search here, start with an @ if you want to search for a user"/>
+                 </div>
+                 <button type="button" className="btn btn-dark searchButton" onClick={() => {setHolder(document.getElementById('search').value)}}>Search</button>
+                 {/* Search for user or company: (search box) (start with an @ for user, otherwise assumes company)*/}
+                 {/* Chart.js components showing sentiment over time */}
+             </div>
+             
+        <div className="chart">
+                <Line width={50} height = {20}
+                data= {{
+            labels: ['10 am', '10:30 am', '11 am', '11:30 am', '12 pm', '12:30 pm'],
+            datasets: [{
+                label: 'Sentiment over time',
+                data: getTheChart(holder),
+                backgroundColor: 'rgba(0, 0, 0, 1)',
+                borderColor: '#004d00',
+                borderWidth: 1
+            }]
+        }}/>
+    </div>
+    </div>
     )
 }

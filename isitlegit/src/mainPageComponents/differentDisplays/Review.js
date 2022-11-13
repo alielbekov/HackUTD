c5b7f7247f08ab525cd46ff644b2ca17b0bd4b9f
rahiblaghari <rlaghari@gmail.com>
2022-11-13 11:47:01 -0600

    changes

--- isitlegit/src/mainPageComponents/differentDisplays/Review.js
+++ isitlegit/src/mainPageComponents/differentDisplays/Review.js
@@ -1,6 +1,23 @@
 import React from "react";
 import {Response} from "../responseMessage.js"
+// 
+import { Doughnut } from 'react-chartjs-2';
+// 
+
 export default function Home(){
+
+  let [positive, setPositive] = React.useState(80)
+  let [negative, setNegative] = React.useState(20)
+
+  function calcValues(){
+    // setPositive(document.getElementById("rangeInput"))
+    // let input = document.getElementById("comments").value
+    let num = document.getElementById("rangeInput").value
+    num*=20
+    setPositive(num)
+    setNegative(100-num)
+  }
+
     return(
       <div className="row d-flex justify-content-center ">
       <div className = "surveyWrapper  m-3 p-3 my-3">
@@ -21,18 +38,43 @@ export default function Home(){
                 <span className="surveyInputValue">4</span>
                 <span>5</span>
                 </div>
-                <input className = "isInput" type="range" min="0" max="5" />
+                <input className = "isInput" id="rangeInput" type="range" min="0" max="5" />
                 <br />
 
                 <div>Your review here: </div>
                 <textarea id="comments" className="textarea"/>
                 <br />
-                <button className="btn btn-success postButton" onClick={()=>{ alert('Thank you for submitting your reviews!'); }}> Submit </button>
+                <button className="btn btn-success postButton" onClick={()=>{ calcValues(); alert('Thank you for submitting your reviews!'); }}> Submit </button>
                 {/* onClick = {() => goToStart()} */}
                 <br />
                 {/* <button onClick={() => validateSignIn()}>Click to sign in</button> */}
           </div>
-          <Response polarity = {"Hello"} />
+          {/* <Response polarity = {"Hello"} /> */}
+          {/* 
+           */}
+           <div>
+  <div className="doughnutData">
+        <Doughnut data = {{
+          labels: [
+            'Positive',
+            'Negative',
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [positive, negative],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+            ],
+            hoverOffset: 4
+          }]
+        }}
+    />
+  </div>
+
+  </div>
+           {/* 
+            */}
       </div>
 
 

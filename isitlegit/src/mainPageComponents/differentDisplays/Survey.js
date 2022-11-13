fc3a0cdd6ba29e983143b68eed978f20d7d38712
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-11-12 22:59:04 -0600

    survey mostly ready

--- isitlegit/src/mainPageComponents/differentDisplays/Survey.js
+++ isitlegit/src/mainPageComponents/differentDisplays/Survey.js
@@ -4,30 +4,34 @@ export default function Home(){
     let [rating, setRatingOne] = React.useState(0)
 
     return(
-        <div className="surveyInput">
-            <div>What is your Name: </div>
-            <input type="text" id="name" className="nameInput" />
-            <div>Leave a Rating:</div>
-            <br />
+      <div className="row d-flex justify-content-center ">
+      <div className = "surveyWrapper  m-3 p-3 my-3">
+            <div className="surveyInput">
+                <div>What is your Name: </div>
+                <input type="text" id="name" className="nameInput" />
+                <div>Leave a Rating:</div>
+                <br />
 
-            {/*}<span class="checked">✭</span>
-            <span onClick={() => (setRatingTwo(true), setRatingThree(false), setRatingFour(false), setRatingFive(false))}>✭</span> */}
-            <div>
-            <span class="surveyInputValue">0</span>
-            <span class="surveyInputValue">1</span>
-            <span class="surveyInputValue">2</span>
-            <span class="surveyInputValue">3</span>
-            <span class="surveyInputValue">4</span>
-            <span class="">5</span>
-            </div>
-            <input className = "isInput" type="range" min="0" max="5" />
-            <div>Any Additional Comments: </div>
-            <textarea id="comments" className="textarea"/>
-            <br />
-            <button className="btn btn-success postButton" > Submit </button>
-            {/* onClick = {() => goToStart()} */}
-            <br />
-            {/* <button onClick={() => validateSignIn()}>Click to sign in</button> */}
+                {/*}<span class="checked">✭</span>
+                <span onClick={() => (setRatingTwo(true), setRatingThree(false), setRatingFour(false), setRatingFive(false))}>✭</span> */}
+                <div>
+                <span class="surveyInputValue">0</span>
+                <span class="surveyInputValue">1</span>
+                <span class="surveyInputValue">2</span>
+                <span class="surveyInputValue">3</span>
+                <span class="surveyInputValue">4</span>
+                <span>5</span>
+                </div>
+                <input className = "isInput" type="range" min="0" max="5" />
+                <div>Any Additional Comments: </div>
+                <textarea id="comments" className="textarea"/>
+                <br />
+                <button className="btn btn-success postButton" > Submit </button>
+                {/* onClick = {() => goToStart()} */}
+                <br />
+                {/* <button onClick={() => validateSignIn()}>Click to sign in</button> */}
+          </div>
+      </div>
       </div>
     )
 }
